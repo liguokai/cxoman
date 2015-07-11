7e999ab3c775da7f56f9f7191623040b35b64619
liguokai <46029755@qq.com>
2015-07-12 01:38:44 +0800

    add authentication logics

--- controllers/default.js
+++ controllers/default.js
@@ -1,7 +1,13 @@
 'use strict';
 exports.install = function() {
+    F.route('/', view_index);
     F.route('#401', error401);
 };
+
+function view_index() {
+    var self = this;
+    self.view('main');
+}
 
 function error401() {
    var self = this;
